4abd0ce52254fdb66f0d2522df3ec7dc958f62b4
Otávio Fernandes <67168562+taviofernandes@users.noreply.github.com>
2026-02-06 14:45:05 -0300

    Delete Tasklist directory

--- Tasklist/src/main/java/dev/otavio/tasklist/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.otavio.tasklist.repository;
-
-import dev.otavio.tasklist.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
